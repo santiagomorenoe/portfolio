e8c9e1e15824ac313abaad12a46497e8f9e7c405
santiagomorenoe <morenoestradasantiago@gmail.com>
2025-08-18 23:00:52 -0600

    update:links added

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,14 @@ export const projects = [
   {
     name: "Apple Iphone 3D Website",
     image: "/iphone.jpeg",
-    link: "https://syntora.com",
+    link: "https://iphonewebsite-clone.netlify.app/",
     tags: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
     isFeatured: true,
   },
   {
     name: "Food Delivery App",
     image: "/foodapp.jpeg",
-    link: "https://syntora.com",
+    link: "",
     tags: [
       "/expo.svg",
       "/appwrite.svg",
@@ -22,14 +22,14 @@ export const projects = [
   {
     name: "AI Translate Web App",
     image: "/translate.jpeg",
-    link: "https://syntora.com",
+    link: "https://syntora-translate-ai.vercel.app/",
     tags: ["/react.svg", "/next.svg", "/js.svg", "/gemini.svg", "/tail.svg"],
     isFeatured: true,
   },
   {
     name: "Web Maker's",
     image: "/webmakers.jpeg",
-    link: "https://syntora.com",
+    link: "https://iwebmakers.netlify.app/",
     tags: ["/next.svg", "/tail.svg", "/ts.svg", "/frm.svg", "/figma.svg"],
     isFeatured: true,
   },
