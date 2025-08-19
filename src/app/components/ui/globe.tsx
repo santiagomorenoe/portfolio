0b3596090ea0f3f22083ed38f6e2afabea45c3ba
santiagomorenoe <morenoestradasantiago@gmail.com>
2025-08-18 22:58:32 -0600

    update: bg color

--- src/app/components/ui/globe.tsx
+++ src/app/components/ui/globe.tsx
@@ -28,7 +28,7 @@ export default function Page() {
         </div>
 
         <div className="mt-14 grid grid-cols-5">
-          <div className="relative -mt-32 h-96 w-screen overflow-hidden [mask-image:radial-gradient(50%_50%,white,transparent)] before:absolute before:inset-0 before:bg-[radial-gradient(circle_at_bottom_center,#e60a64,transparent_70%)] before:opacity-40 after:absolute after:top-1/2 after:-left-1/2 after:aspect-[1/0.7] after:w-[200%] after:rounded-[100%] after:border-t after:border-[#c5769066] after:bg-zinc-900">
+          <div className="relative -mt-32 h-96 w-screen overflow-hidden [mask-image:radial-gradient(50%_50%,white,transparent)] before:absolute before:inset-0 before:bg-[radial-gradient(circle_at_bottom_center,var(--color-secondary),transparent_70%)] before:opacity-40 after:absolute after:top-1/2 after:-left-1/2 after:aspect-[1/0.7] after:w-[200%] after:rounded-[100%] after:border-t after:border-[#c5769066] after:bg-zinc-900">
           </div>
         </div>
       </div>
